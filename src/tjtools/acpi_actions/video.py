71e97137dc529e7a6f5b38e99e554ff0b55befb5
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2024-08-19 20:42:54 +0200

    acpi_actions: migrate video to new brightness daemon

--- src/tjtools/acpi_actions/video.py
+++ src/tjtools/acpi_actions/video.py
@@ -6,9 +6,13 @@
 # Imports
 ##########################################################################################
 
+from ctypes import addressof, c_uint8, c_int32, memmove, sizeof, Structure
+from enum import IntEnum
+from json import loads as jloads
 from logging import Logger
+from pathlib import Path
+from socket import socket, AF_UNIX, SOCK_DGRAM
 
-from ..brightness import modify_state as modify_brightness
 from .common import ActionConfig
 
 
@@ -18,6 +22,72 @@ from .common import ActionConfig
 
 _subsystem = 'video'
 _log_prefix = f'ACPI: {_subsystem}: '
+
+_config_path = Path('/etc/brightness-daemon.conf')
+
+
+##########################################################################################
+# Enumerator definitions
+##########################################################################################
+
+class CommandType(IntEnum):
+    SetState     = 0
+    ModifyState  = 1
+    SaveState    = 2
+    RestoreState = 3
+    SetPowersave = 4
+
+
+##########################################################################################
+# C-structs
+##########################################################################################
+
+class ModifyStateFrame(Structure):
+    _pack_ = 1
+    _fields_ = (
+        ('type', c_uint8),
+        ('len', c_uint8),
+        ('value', c_int32),
+    )
+
+    def serialize(self) -> bytearray:
+        self_size = sizeof(ModifyStateFrame)
+        buf = (c_uint8 * self_size)()
+
+        memmove(addressof(buf), addressof(self), self_size)
+
+        return bytearray(buf)
+
+
+##########################################################################################
+# Internal functions
+##########################################################################################
+
+def _modify_brightness(value: int) -> None:
+    '''
+    Modify the backlight brightness.
+
+    Arguments:
+        value - signed value to apply to current brightness
+    '''
+
+    config_data = jloads(_config_path.read_text(encoding='utf8'))
+
+    client = socket(AF_UNIX, SOCK_DGRAM)
+
+    socket_path = config_data.get('socket-path')
+    if socket_path is None or not Path(socket_path).is_socket():
+        return
+
+    client.connect(socket_path)
+
+    frame = ModifyStateFrame()
+
+    frame.type  = CommandType.ModifyState
+    frame.len   = 4
+    frame.value = value
+
+    client.sendall(frame.serialize())
 
 
 ##########################################################################################
@@ -35,17 +105,19 @@ def handle_event(lg: Logger, cfg: ActionConfig, action: str, device: str) -> int
     '''
 
     if action == 'brightnessdown' and device == 'BRTDN':
-        value = str(-cfg.brightness_modifier)
+        value = -cfg.brightness_modifier
     elif action == 'brightnessup' and device == 'BRTUP':
-        value = str(cfg.brightness_modifier)
+        value = cfg.brightness_modifier
     else:
         lg.error(_log_prefix + f'unknown action/device: {action}/{device}')
 
         return 1
 
-    ret = modify_brightness(lg, value)
-    if ret != 0:
-        lg.error(_log_prefix + f'failed to modify brightness: {ret}')
+    try:
+        _modify_brightness(value)
+
+    except Exception as exc:
+        lg.error(_log_prefix + f'failed to modify brightness: {exc}')
 
         return 2
 
